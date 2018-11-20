fca83c6a5b823cde63c0328f6b01fbc5f49d62fd
Alexey Nesterenko <lex.kaname@gmail.com>
2018-11-20 12:51:36 +0300

    For Group minimal model response

---
+++ VKApi/Model/GroupModelMin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKApi.Model
+{
+    public class GroupModelMin
+    {
+        public Response response { get; set; }
+
+        public class Response
+        {
+            public int count { get; set; }
+            public int[] items { get; set; }
+        }
+    }
+}
